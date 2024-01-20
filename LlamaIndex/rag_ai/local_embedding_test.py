f30aa98c97f1a0bc49c32f22c0a89034340b060d
Jianshao <787903+jianshaow@users.noreply.github.com>
2024-01-20 22:02:26 +0800

    fix typo

--- LlamaIndex/rag_ai/local_embedding_test.py
+++ LlamaIndex/rag_ai/local_embedding_test.py
@@ -1,5 +1,8 @@
 from llama_index.embeddings import HuggingFaceEmbedding
 
-embed_model = HuggingFaceEmbedding()
+embed_model = HuggingFaceEmbedding(
+    model_name="BAAI/bge-large-en-v1.5",
+    text_instruction="Represent this sentence for searching relevant passages:",
+)
 embeddings = embed_model.get_text_embedding("What did the author do growing up?")
 print(len(embeddings))
