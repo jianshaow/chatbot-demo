f13cb586b005b0959f5c79cee6eb6a58cc36513c
Jianshao <787903+jianshaow@users.noreply.github.com>
2024-01-21 13:43:08 +0800

    use default embeding model

--- LlamaIndex/rag_chat/local_embedding_test.py
+++ LlamaIndex/rag_chat/local_embedding_test.py
@@ -1,8 +1,5 @@
 from llama_index.embeddings import HuggingFaceEmbedding
 
-embed_model = HuggingFaceEmbedding(
-    model_name="BAAI/bge-large-en-v1.5",
-    text_instruction="Represent this sentence for searching relevant passages:",
-)
+embed_model = HuggingFaceEmbedding()
 embeddings = embed_model.get_text_embedding("What did the author do growing up?")
 print(len(embeddings))
