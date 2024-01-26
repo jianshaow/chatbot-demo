9d4c3ea0e4e41cf309e690600e90935166ea8a1c
Jianshao <787903+jianshaow@users.noreply.github.com>
2024-01-26 13:56:58 +0000

    fix typo

--- LlamaIndex/test/chroma_clean.py
+++ LlamaIndex/test/chroma_clean.py
@@ -3,6 +3,6 @@ import sys, chromadb
 db = chromadb.PersistentClient(path="LlamaIndex/chroma_db")
 collection = len(sys.argv) == 2 and sys.argv[1] or None
 if collection is None:
-    print("provide the collectino name")
+    print("provide the collection name")
 else:
     db.delete_collection(collection)
