4b9af07824753ebdad89ae8a6ebcfca0e5bfea09
Jianshao <787903+jianshaow@users.noreply.github.com>
2024-02-28 15:16:26 +0800

    uplift llama-index

--- LlamaIndex/rag_chat/indexer.py
+++ LlamaIndex/rag_chat/indexer.py
@@ -1,6 +1,6 @@
 import chromadb, rag_config
 from llama_index.core import (
-    ServiceContext,
+    Settings,
     StorageContext,
     VectorStoreIndex,
     SimpleDirectoryReader,
@@ -12,24 +12,19 @@ config = rag_config.get_config()
 db = chromadb.PersistentClient(path=config.vector_db_path)
 chroma_collection = db.get_or_create_collection(config.vector_db_collection)
 vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
-service_context = ServiceContext.from_defaults(
-    embed_model=config.embedding_model(), llm=None
-)
-print("embed_model:", service_context.embed_model.model_name)
+Settings.embed_model = config.embedding_model()
+print("embed_model:", Settings.embed_model.model_name)
+
 if chroma_collection.count() == 0:
     documents = SimpleDirectoryReader(config.data_path).load_data(show_progress=True)
     storage_context = StorageContext.from_defaults(vector_store=vector_store)
     index = VectorStoreIndex.from_documents(
         documents,
         storage_context=storage_context,
-        service_context=service_context,
         show_progress=True,
     )
 else:
-    index = VectorStoreIndex.from_vector_store(
-        vector_store,
-        service_context=service_context,
-    )
+    index = VectorStoreIndex.from_vector_store(vector_store)
 
 retriever = index.as_retriever()
 question = config.get_question()
--- LlamaIndex/rag_chat/queryer.py
+++ LlamaIndex/rag_chat/queryer.py
@@ -1,20 +1,16 @@
 import chromadb, rag_config
-from llama_index.core import ServiceContext, VectorStoreIndex
+from llama_index.core import Settings, VectorStoreIndex
 from llama_index.vector_stores.chroma import ChromaVectorStore
 
 config = rag_config.get_config()
 
+Settings.embed_model = config.embedding_model()
+Settings.llm = config.chat_model()
+
 db = chromadb.PersistentClient(path=config.vector_db_path)
 chroma_collection = db.get_or_create_collection(config.vector_db_collection)
-
 vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
-service_context = ServiceContext.from_defaults(
-    embed_model=config.embedding_model(),
-    llm=config.chat_model(),
-)
-index = VectorStoreIndex.from_vector_store(
-    vector_store, service_context=service_context
-)
+index = VectorStoreIndex.from_vector_store(vector_store)
 
 query_engine = index.as_query_engine(streaming=True)
 question = config.get_question()
