1540fc1814aca98cc76207596a50684ee47df618
Jianshao <787903+jianshaow@users.noreply.github.com>
2024-03-19 10:14:38 +0000

    update

--- llama-cpp/docker-entrypoint.sh
+++ llama-cpp/docker-entrypoint.sh
@@ -21,5 +21,7 @@ if [ "${CUDA_VERSION}" != "" ]; then
   GPU_ARGS="--n_gpu_layers -1"
 fi
 
+model_path = $(huggingface-cli download $HF_REPO_ID $HF_MODEL_FILE)
+
 python3 -m llama_cpp.server --host 0.0.0.0 --chat_format chatml $GPU_ARGS $LC_ARGS \
-       --hf_model_repo_id $HF_REPO_ID --model $HF_MODEL_FILE --model_alias $HF_MODEL_ALIAS
+        --model $model_path --model_alias $HF_MODEL_ALIAS
