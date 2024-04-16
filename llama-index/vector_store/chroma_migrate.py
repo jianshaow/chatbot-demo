de7de9b87cf2140613a0fc87a677bd0b03c6616b
Jianshao <787903+jianshaow@users.noreply.github.com>
2024-04-16 08:36:12 +0000

    enhance migration tool

--- llama-index/vector_store/chroma_migrate.py
+++ llama-index/vector_store/chroma_migrate.py
@@ -1,16 +1,33 @@
 import os, sys, chromadb
 
 if len(sys.argv) != 3:
-    print("input source and target collection name")
+    print("input source and target, format: <collection>@<db_path>")
 else:
-    source = sys.argv[1]
-    target = sys.argv[2]
+    src = sys.argv[1]
+    tgt = sys.argv[2]
     path = os.environ.get("CHROMA_DB_DIR", "chroma")
-    db = chromadb.PersistentClient(path=path)
-    source_collection = db.get_collection(source)
-    target_collection = db.get_or_create_collection(target)
-    vectors = source_collection.peek(source_collection.count())
-    target_collection.add(
+
+    src_list = src.split("@")
+    src_name = src_list[0]
+    if len(src_list) == 1:
+        src_path = path
+    else:
+        src_path = src_list[1]
+
+    tgt_list = tgt.split("@")
+    tgt_name = tgt_list[0]
+    if len(tgt_list) == 1:
+        tgt_path = path
+    else:
+        tgt_path = tgt_list[1]
+
+    src_db = chromadb.PersistentClient(path=src_path)
+    tgt_db = chromadb.PersistentClient(path=tgt_path)
+
+    src_collection = src_db.get_collection(src_name)
+    tgt_collection = tgt_db.get_or_create_collection(tgt_name)
+    vectors = src_collection.peek(src_collection.count())
+    tgt_collection.add(
         ids=vectors["ids"],
         embeddings=vectors["embeddings"],
         documents=vectors["documents"],
@@ -19,4 +36,4 @@ else:
         images=vectors["data"],
     )
 
-    print("data migrated:", target_collection.count())
+    print("data migrated:", tgt_collection.count())
