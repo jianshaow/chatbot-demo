fa05a2fdb733a24be2300022a754b3d079ed4d61
Jianshao <787903+jianshaow@users.noreply.github.com>
2024-04-16 06:17:08 +0000

    add args to specify collection

--- llama-index/vector_store/vector_store_test.py
+++ llama-index/vector_store/vector_store_test.py
@@ -1,10 +1,11 @@
-import os, chromadb
+import os, sys, chromadb
 from llama_index.core.vector_stores import VectorStoreQuery
 from llama_index.vector_stores.chroma import ChromaVectorStore
 
 path = os.environ.get("CHROMA_DB_DIR", "chroma")
 db = chromadb.PersistentClient(path=path)
-chroma_collection = db.get_or_create_collection("hface")
+collection = len(sys.argv) == 2 and sys.argv[1] or "hface"
+chroma_collection = db.get_or_create_collection(collection)
 vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
 
 vectors = chroma_collection.peek(1)
