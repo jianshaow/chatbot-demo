ca91a4d43d67915eeeee1c7414b0caeab4894066
Jianshao <787903+jianshaow@users.noreply.github.com>
2024-04-18 13:51:42 +0000

    add prompt and bnb quantization

--- transformers/causallm.py
+++ transformers/causallm.py
@@ -1,19 +1,33 @@
-from transformers import (
-    AutoModelForCausalLM,
-    AutoTokenizer,
-)
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
 
 model_name = "lmsys/vicuna-7b-v1.5"
-tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+quantization_config = BitsAndBytesConfig(
+    load_in_4bit=True, bnb_4bit_compute_dtype=torch.float16
+)
+
 model = AutoModelForCausalLM.from_pretrained(
     model_name,
-    trust_remote_code=True,
+    quantization_config=quantization_config,
     device_map="auto",
 )
 
-prompt = "hello"
-inputs = tokenizer.encode(prompt, return_tensors="pt").to(model.device)
-outputs = model.generate(inputs)
-response = tokenizer.decode(outputs[0])
+prompt = """[INST]<<SYS>>
+You are an AI assistant that answers questions in a friendly manner, based on the given source documents. Here are some rules you always follow:
+- Generate human readable output, avoid creating output with gibberish text.
+- Generate only the requested output, don't include any other language before or after the requested output.
+- Never say thank you, that you are happy to help, that you are an AI agent, etc. Just answer directly.
+- Generate professional language typically used in business documents in North America.
+- Never generate offensive or foul language.
+<</SYS>
+
+who are you?[/INST]
+"""
+inputs = tokenizer(prompt, return_tensors="pt").to(model.device)
+tokens = model.generate(**inputs)
+completion_tokens = tokens[0][inputs["input_ids"].size(1) :]
+response = tokenizer.decode(completion_tokens, skip_special_tokens=True)
 
 print(response)
