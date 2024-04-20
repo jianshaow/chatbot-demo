6625685d1f58f9da374f185e3852a477f2a2a39f
Jianshao <787903+jianshaow@users.noreply.github.com>
2024-04-20 14:20:06 +0000

    use common prompt

--- transformers/causallm_demo.py
+++ transformers/causallm_demo.py
@@ -1,4 +1,4 @@
-import os, sys, torch
+import os, sys, torch, prompts
 from transformers import (
     AutoModelForCausalLM,
     AutoTokenizer,
@@ -8,7 +8,6 @@ from transformers import (
 
 model_name = os.environ.get("HF_MODEL_NAME", "lmsys/vicuna-7b-v1.5")
 tokenizer = AutoTokenizer.from_pretrained(model_name)
-
 
 quantization_config = None
 if os.environ.get("BNB_ENABLED", "false") == "true":
@@ -23,19 +22,7 @@ model = AutoModelForCausalLM.from_pretrained(
 )
 
 query_str = len(sys.argv) == 2 and sys.argv[1] or "Who are you?"
-
-prompt = f"""[INST]<<SYS>>
-You are an AI assistant that answers questions in a friendly manner, based on the given source documents. Here are some rules you always follow:
-- Generate human readable output, avoid creating output with gibberish text.
-- Generate only the requested output, don't include any other language before or after the requested output.
-- Never say thank you, that you are happy to help, that you are an AI agent, etc. Just answer directly.
-- Generate professional language typically used in business documents in North America.
-- Never generate offensive or foul language.
-<</SYS>
-
-{query_str}[/INST]
-"""
-
+prompt = prompts.chat_prompt(query_str)
 input_ids = tokenizer(
     prompt, return_tensors="pt", return_attention_mask=False
 ).input_ids.to(model.device)
