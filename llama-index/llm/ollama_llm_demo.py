4a48a5f7e1d4d8cd491fe3e90522d890bd797f6f
Jianshao <787903+jianshaow@users.noreply.github.com>
2024-04-26 03:38:14 +0000

    add env

--- llama-index/llm/ollama_llm_demo.py
+++ llama-index/llm/ollama_llm_demo.py
@@ -1,7 +1,10 @@
+import os
 from llama_index.llms.ollama import Ollama
 from llama_index.core.llms import ChatMessage
 
-llm = Ollama(base_url="http://host.docker.internal:11434", model="vicuna:13b-v1.5-q4_0")
+base_url = os.environ.get("OLLAMA_BASE_URL", "http://host.docker.internal:11434")
+model = os.environ.get("OLLAMA_MODEL", "vicuna:13b")
+llm = Ollama(base_url=base_url, model=model)
 
 messages = [
     ChatMessage(role="system", content="You are a pirate with a colorful personality"),
