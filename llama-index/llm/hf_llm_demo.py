f02d873395d24854b3696c7658dc33ad0daf4a4b
Jianshao <787903+jianshaow@users.noreply.github.com>
2024-04-26 08:21:14 +0000

    add printing

--- llama-index/llm/hf_llm_demo.py
+++ llama-index/llm/hf_llm_demo.py
@@ -1,19 +1,6 @@
 import os, torch
 from transformers import BitsAndBytesConfig
 from llama_index.llms.huggingface import HuggingFaceLLM
-from llama_index.core import PromptTemplate
-
-SYSTEM_PROMPT = """You are an AI assistant that answers questions in a friendly manner, based on the given source documents. Here are some rules you always follow:
-- Generate human readable output, avoid creating output with gibberish text.
-- Generate only the requested output, don't include any other language before or after the requested output.
-- Never say thank you, that you are happy to help, that you are an AI agent, etc. Just answer directly.
-- Generate professional language typically used in business documents in North America.
-- Never generate offensive or foul language.
-"""
-
-query_wrapper_prompt = PromptTemplate(
-    "[INST]<<SYS>>\n" + SYSTEM_PROMPT + "<</SYS>>\n\n{query_str}[/INST] "
-)
 
 model_name = os.environ.get("HF_MODEL_NAME", "lmsys/vicuna-7b-v1.5")
 
@@ -28,11 +15,14 @@ llm = HuggingFaceLLM(
     context_window=4096,
     max_new_tokens=2048,
     # generate_kwargs={"temperature": 0.0, "do_sample": False},
-    query_wrapper_prompt=query_wrapper_prompt,
     tokenizer_name=model_name,
     model_name=model_name,
     model_kwargs=model_kwargs,
+    system_prompt="You are a pirate with a colorful personality.",
+    query_wrapper_prompt="USER: {query_str}",
 )
 
-response = llm.complete("who are you")
+print("-" * 80)
+response = llm.complete("who are you?")
 print(response.text)
+print("\n" + "-" * 80)
--- llama-index/llm/ollama_llm_demo.py
+++ llama-index/llm/ollama_llm_demo.py
@@ -10,7 +10,8 @@ messages = [
     ChatMessage(role="system", content="You are a pirate with a colorful personality"),
     ChatMessage(role="user", content="What is your name"),
 ]
+print("-" * 80)
 resp = llm.stream_chat(messages)
 for r in resp:
     print(r.delta, end="")
-print("\n-----------------------------------")
+print("\n" + "-" * 80)
