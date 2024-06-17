ce6f9ab789a2898ac5d9b6526e24c0c52039de60
Jianshao <787903+jianshaow@users.noreply.github.com>
2024-06-17 10:39:28 +0000

    add default model

--- llm-api/embedding/ollama_demo.py
+++ llm-api/embedding/ollama_demo.py
@@ -1,6 +1,7 @@
-import sys, ollama
+import os, sys, ollama
 
-question = len(sys.argv) == 2 and sys.argv[1] or "What did the author do growing up?"
-embeddings = ollama.embeddings(model="nomic-embed-text:v1.5", prompt=question)["embedding"]
+model = os.environ.get("OLLAMA_MODEL", "nomic-embed-text:v1.5")
+prompt = len(sys.argv) == 2 and sys.argv[1] or "What did the author do growing up?"
+embeddings = ollama.embeddings(model=model, prompt=prompt)["embedding"]
 print(len(embeddings))
 print(embeddings[:4])
