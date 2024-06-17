3024ac7bd53d006548ee5a6903c6b2de8a4581a1
Jianshao <787903+jianshaow@users.noreply.github.com>
2024-06-17 15:23:18 +0000

    add openai zh retriever demo

---
+++ llama-index/retriever/openai_zh_demo.py
@@ -0,0 +1,16 @@
+import sys
+from llama_index.core import Settings, VectorStoreIndex, SimpleDirectoryReader
+
+documents = SimpleDirectoryReader("data_zh").load_data(show_progress=True)
+index = VectorStoreIndex.from_documents(
+    documents,
+    show_progress=True,
+)
+print("embed_model:", Settings.embed_model.model_name)
+
+retriever = index.as_retriever()
+question = len(sys.argv) == 2 and sys.argv[1] or "地球发动机都安装在哪里？"
+nodes = retriever.retrieve(question)
+for node in nodes:
+    print("---------------------------------------------")
+    print(node)
