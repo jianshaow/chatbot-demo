80a44e42f7faee63097d985380344424318daf8b
Jianshao <787903+jianshaow@users.noreply.github.com>
2024-06-17 14:44:27 +0000

    add ollama demo

---
+++ langchain/retriever/ollama_demo.py
@@ -0,0 +1,24 @@
+import os, sys
+
+from langchain_community.document_loaders import DirectoryLoader
+from langchain_text_splitters import CharacterTextSplitter
+from langchain_chroma import Chroma
+from langchain_community.embeddings import OllamaEmbeddings
+
+loader = DirectoryLoader("data")
+data = loader.load()
+
+text_splitter = CharacterTextSplitter()
+documents = text_splitter.split_documents(data)
+
+base_url = os.environ.get("OLLAMA_BASE_URL", "http://host.docker.internal:11434")
+model = os.environ.get("OLLAMA_MODEL", "nomic-embed-text:v1.5")
+vectorstore = Chroma.from_documents(
+    documents=documents, embedding=OllamaEmbeddings(base_url=base_url, model=model)
+)
+question = len(sys.argv) == 2 and sys.argv[1] or "What did the author do growing up?"
+retriever = vectorstore.as_retriever()
+docs = retriever.invoke(question)
+for doc in docs:
+    print("-" * 80)
+    print(doc.page_content[:80])
