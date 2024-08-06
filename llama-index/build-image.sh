73cf67f822130bcf8f3c01406436222439d61ebd
Jianshao <787903+jianshaow@users.noreply.github.com>
2024-08-06 17:45:39 +0800

    change tag

--- llama-index/build-image.sh
+++ llama-index/build-image.sh
@@ -6,7 +6,7 @@ fi
 echo "Using torch version: ${torch_ver}"
 
 docker build -t jianshao/llamaindex-demo:latest . \
-       --build-arg TAG=$torch_ver-gpu
+       --build-arg TAG=$torch_ver
 
 llamaindex_ver=$(docker run --rm jianshao/llamaindex-demo:latest pip list | grep llama-index-core|awk '{print $2}')
 echo "Using llama-index version ${llamaindex_ver}"
