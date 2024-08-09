0488fcabb4e73274e7789ad17efb7a027e7e8749
Jianshao <787903+jianshaow@users.noreply.github.com>
2024-08-09 08:08:21 +0000

    complete function call

--- llm-api/openai/fn_call_demo.py
+++ llm-api/openai/fn_call_demo.py
@@ -2,12 +2,10 @@ import os, json, openai
 
 
 def multiply(a: int, b: int) -> int:
-    print("multiply:", a, b)
     return a * b
 
 
 def add(a: int, b: int) -> int:
-    print("add:", a, b)
     return a + b
 
 
@@ -56,34 +54,27 @@ fns = {
 }
 
 messages = [
-    {
-        "role": "system",
-        "content": "You are a helpful customer support assistant. Use the supplied tools to assist the user.",
-    },
     {"role": "user", "content": "What is (121 * 3) + 42?"},
 ]
 
 model = os.environ.get("OPENAI_FC_MODEL", "gpt-4o-mini")
-response = openai.chat.completions.create(
-    model=model, messages=messages, tools=tools
-)
-print("response:", response)
+response = openai.chat.completions.create(model=model, messages=messages, tools=tools)
 
-messages.append(response.choices[0].message)
-
-for tool_call in response.choices[0].message.tool_calls:
-    fn = fns[tool_call.function.name]
-    fn_args = json.loads(tool_call.function.arguments)
-    fn_response = fn(**fn_args)
-    messages.append(
-        {
-            "role": "tool",
-            "content": str(fn_response),
-            "tool_call_id": tool_call.id,
-        }
+while response.choices[0].finish_reason == "tool_calls":
+    messages.append(response.choices[0].message)
+    for tool_call in response.choices[0].message.tool_calls:
+        fn = fns[tool_call.function.name]
+        fn_args = json.loads(tool_call.function.arguments)
+        fn_response = fn(**fn_args)
+        messages.append(
+            {
+                "role": "tool",
+                "content": str(fn_response),
+                "tool_call_id": tool_call.id,
+            }
+        )
+    response = openai.chat.completions.create(
+        model=model, messages=messages, tools=tools
     )
-
-print("messges:", messages)
-response = openai.chat.completions.create(model=model, messages=messages)
 
 print(response.choices[0].message.content)
