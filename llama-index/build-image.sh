d6fa7201a28376ffae60bc8796004adada496f7f
Jianshao <787903+jianshaow@users.noreply.github.com>
2024-09-12 06:34:09 +0000

    improve

--- llama-index/build-image.sh
+++ llama-index/build-image.sh
@@ -5,14 +5,18 @@ if [ "$torch_ver" == "" ]; then
 fi
 echo "Using torch version: ${torch_ver}"
 
-docker build -t jianshao/llamaindex-demo:latest . \
+base_image=jianshao/torch-dev-base
+docker pull ${base_image}:${torch_ver}
+
+image=jianshao/llamaindex-demo
+docker build -t ${image}:latest . \
        --build-arg TAG=$torch_ver
 
-llamaindex_ver=$(docker run --rm jianshao/llamaindex-demo:latest pip list | grep llama-index-core| awk '{print $2}')
+llamaindex_ver=$(docker run --rm ${image}:latest pip list | grep llama-index-core | awk '{print $2}')
 echo "Using llama-index version ${llamaindex_ver}"
 
-docker tag jianshao/llamaindex-demo:latest jianshao/llamaindex-demo:${llamaindex_ver}
-docker push jianshao/llamaindex-demo:latest
-docker push jianshao/llamaindex-demo:${llamaindex_ver}
+docker tag ${image}:latest ${image}:${llamaindex_ver}
+docker push ${image}:latest
+docker push ${image}:${llamaindex_ver}
 
 echo "Done"
