57a468445e340c2d8cb6df1ce8459810bfc8b6e1
Jianshao <787903+jianshaow@users.noreply.github.com>
2024-09-18 05:25:58 +0000

    add args

--- langchain/build-image.sh
+++ langchain/build-image.sh
@@ -9,7 +9,7 @@ base_image=jianshao/torch-dev-base
 docker pull ${base_image}:${torch_ver}
 
 image=jianshao/langchain-demo
-docker build -t ${image}:latest . --build-arg TAG=$torch_ver
+docker build -t ${image}:latest . --build-arg TAG=$torch_ver $*
 
 langchain_core_ver=$(docker run --rm ${image}:latest pip list | grep langchain-core | awk '{print $2}')
 echo "Using langchain-core version ${langchain_core_ver}"
