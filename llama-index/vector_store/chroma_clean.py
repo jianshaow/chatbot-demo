40b2a5be01d6f6fcaf5109c35093c6c031472fbc
Jianshao <787903+jianshaow@users.noreply.github.com>
2024-09-20 16:36:59 +0800

    rename

--- llama-index/vector_store/chroma_clean.py
+++
@@ -1,9 +0,0 @@
-import os, sys, chromadb
-
-path = os.environ.get("CHROMA_DB_DIR", "chroma")
-db = chromadb.PersistentClient(path=path)
-collection = len(sys.argv) == 2 and sys.argv[1] or None
-if collection is None:
-    print("provide the collection name")
-else:
-    db.delete_collection(collection)
