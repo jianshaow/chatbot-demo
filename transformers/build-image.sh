032dc8582d15cfe98dfc50c6bb16dae72db03e67
Jianshao <787903+jianshaow@users.noreply.github.com>
2024-09-27 16:33:52 +0000

    uplift

--- transformers/build-image.sh
+++ transformers/build-image.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 if [ "$torch_ver" == "" ]; then
-    torch_ver=2.3.1
+    torch_ver=2.4.1
 fi
 echo "Using torch version: ${torch_ver}"
 
-docker build -t jianshao/transformers-demo:latest . \
-       --build-arg TAG=$torch_ver
+image=jianshao/transformers-demo
+docker build -t ${image}:latest . --build-arg TAG=$torch_ver
 
-transformers_ver=$(docker run --rm jianshao/transformers-demo:latest pip list | grep transformers | awk '{print $2}')
+transformers_ver=$(docker run --rm ${image}:latest pip list | grep transformers | awk '{print $2}')
 echo "Using transformers version ${transformers_ver}"
 
-docker tag jianshao/transformers-demo:latest jianshao/transformers-demo:${transformers_ver}
-docker push jianshao/transformers-demo:latest
-docker push jianshao/transformers-demo:${transformers_ver}
+docker tag ${image}:latest ${image}:${transformers_ver}
+docker push ${image}:latest
+docker push ${image}:${transformers_ver}
 
 echo "Done"
