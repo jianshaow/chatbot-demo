6b1b1678a5ef8165db6b32ac6605e6864b7da9be
Jianshao <787903+jianshaow@users.noreply.github.com>
2024-09-28 05:24:34 +0000

    change default model

--- llamaindex/common/__init__.py
+++ llamaindex/common/__init__.py
@@ -11,4 +11,4 @@ ollama_mm_model = os.getenv("OLLAMA_MM_MODEL", "llava:13b")
 ollama_fc_model = os.getenv("OLLAMA_FC_MODEL", "llama3.1:8b")
 
 hf_embed_model = os.getenv("HF_EMBED_MODEL", "BAAI/bge-small-en")
-hf_chat_model = os.getenv("HF_CHAT_MODEL", "lmsys/vicuna-7b-v1.5")
+hf_chat_model = os.getenv("HF_CHAT_MODEL", "meta-llama/Llama-3.2-3B-Instruct")
