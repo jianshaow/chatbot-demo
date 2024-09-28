907d76dfff66b365d3e21b3d085b5b366eda11aa
Jianshao <787903+jianshaow@users.noreply.github.com>
2024-09-28 14:48:07 +0000

    run on gpu

--- transformers/pipeline_demo.py
+++ transformers/pipeline_demo.py
@@ -1,11 +1,9 @@
-import torch
 from transformers import pipeline
 from common import hf_chat_model as model
 
 pipe = pipeline(
     "text-generation",
     model=model,
-    torch_dtype=torch.bfloat16,
     device_map="auto",
 )
 
