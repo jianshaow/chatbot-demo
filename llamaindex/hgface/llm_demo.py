1ed40797b9acfee0cd5702f0edb25122a781e1cd
Jianshao <787903+jianshaow@users.noreply.github.com>
2024-09-29 09:22:32 +0000

    update

--- llamaindex/hgface/llm_demo.py
+++ llamaindex/hgface/llm_demo.py
@@ -1,6 +1,7 @@
 import os, torch
 from transformers import BitsAndBytesConfig
 from llama_index.llms.huggingface import HuggingFaceLLM
+from llama_index.core.llms import ChatMessage
 
 from common import hf_chat_model as model_name
 
@@ -17,13 +18,16 @@ llm = HuggingFaceLLM(
     tokenizer_name=model_name,
     model_name=model_name,
     model_kwargs=model_kwargs,
-    system_prompt="You are a pirate with a colorful personality.",
 )
 print("-" * 80)
 print("chat model:", model_name)
 
-resp = llm.stream_complete("What is your name?")
+messages = [
+    ChatMessage(role="system", content="You are a pirate with a colorful personality."),
+    ChatMessage(role="user", content="What is your name?"),
+]
 print("-" * 80)
+resp = llm.stream_chat(messages)
 for r in resp:
     print(r.delta, end="")
 print("\n", "-" * 80, sep="")
