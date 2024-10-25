ffadde16e610217d89fc063908ab03c168d1c1f3
Jianshao <787903+jianshaow@users.noreply.github.com>
2024-10-25 17:01:54 +0000

    change default model

--- llamaindex/common/__init__.py
+++ llamaindex/common/__init__.py
@@ -10,7 +10,7 @@ ollama_base_url = os.getenv("OLLAMA_BASE_URL", f"http://{ollama_host}:11434")
 ollama_embed_model = os.getenv("OLLAMA_EMBED_MODEL", "nomic-embed-text:v1.5")
 ollama_chat_model = os.getenv("OLLAMA_CHAT_MODEL", "gemma2:9b")
 ollama_mm_model = os.getenv("OLLAMA_MM_MODEL", "llava:13b")
-ollama_fc_model = os.getenv("OLLAMA_FC_MODEL", "llama3.1:8b")
+ollama_fc_model = os.getenv("OLLAMA_FC_MODEL", "nemotron-mini:4b")
 
 hf_embed_model = os.getenv("HF_EMBED_MODEL", "BAAI/bge-small-en")
 hf_chat_model = os.getenv("HF_CHAT_MODEL", "meta-llama/Llama-3.2-3B-Instruct")
--- llm-api/common/__init__.py
+++ llm-api/common/__init__.py
@@ -3,12 +3,12 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-ollama_host = os.environ.get("OLLAMA_HOST", "localhost")
-ollama_base_url = os.environ.get("OLLAMA_BASE_URL", f"http://{ollama_host}:11434")
-ollama_embed_model = os.environ.get("OLLAMA_EMBED_MODEL", "nomic-embed-text:v1.5")
-ollama_chat_model = os.environ.get("OLLAMA_CHAT_MODEL", "llama3.2:3b")
-ollama_mm_model = os.environ.get("OLLAMA_MM_MODEL", "llava:7b")
-ollama_fc_model = os.environ.get("OLLAMA_FC_MODEL", "llama3.1:8b")
+ollama_host = os.getenv("OLLAMA_HOST", "localhost")
+ollama_base_url = os.getenv("OLLAMA_BASE_URL", f"http://{ollama_host}:11434")
+ollama_embed_model = os.getenv("OLLAMA_EMBED_MODEL", "nomic-embed-text:v1.5")
+ollama_chat_model = os.getenv("OLLAMA_CHAT_MODEL", "gemma2:9b")
+ollama_mm_model = os.getenv("OLLAMA_MM_MODEL", "llava:13b")
+ollama_fc_model = os.getenv("OLLAMA_FC_MODEL", "nemotron-mini:4b")
 
 openai_embed_model = os.getenv("OPENAI_EMBED_MODEL", "text-embedding-ada-002")
 openai_chat_model = os.getenv("OPENAI_CHAT_MODEL", "gpt-4o-mini")
