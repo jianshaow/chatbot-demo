151d34c4e8622a9c398e9d6baa38d2d9b10e4f23
Jianshao <787903+jianshaow@users.noreply.github.com>
2024-11-26 16:24:33 +0000

    replace deprecated method

--- llm-api/ollama/embedding_demo.py
+++ llm-api/ollama/embedding_demo.py
@@ -4,9 +4,11 @@ from common import ollama_embed_model as model_name
 
 print("-" * 80)
 print("embed model:", model_name)
-prompt = len(sys.argv) == 2 and sys.argv[1] or "What did the author do growing up?"
-embeddings = ollama.embeddings(model=model_name, prompt=prompt)["embedding"]
+
+input = len(sys.argv) == 2 and sys.argv[1] or "What did the author do growing up?"
+embedding = ollama.embed(model=model_name, input=input)["embeddings"][0]
+
 print("-" * 80)
-print("dimension:", len(embeddings))
-print(embeddings[:4])
+print("dimension:", len(embedding))
+print(embedding[:4])
 print("-" * 80, sep="")
