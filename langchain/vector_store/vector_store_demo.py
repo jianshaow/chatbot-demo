f16f8c885b941942a5b9f9756b9c6b42bee22a6a
Jianshao <787903+jianshaow@users.noreply.github.com>
2024-11-27 10:22:03 +0000

    update chroma tools

--- langchain/vector_store/vector_store_demo.py
+++ langchain/vector_store/vector_store_demo.py
@@ -1,27 +1,34 @@
 import os, sys, chromadb
 from langchain_chroma import Chroma
 
-path = os.environ.get("CHROMA_DB_DIR", "chroma")
-db = chromadb.PersistentClient(path=path)
-collection = len(sys.argv) == 2 and sys.argv[1] or "ollama"
-chroma_collection = db.get_or_create_collection(collection)
+base_dir = os.getenv("CHROMA_BASE_DIR", "chroma")
 
-vectors = chroma_collection.peek(1)
-vector = vectors["embeddings"][0]
-result = chroma_collection.query(vector, n_results=2)
-print("-" * 33, "chroma query", "-" * 33)
-for i in range(len(result["ids"][0])):
-    print("Text:", result["documents"][0][i][:60])
-    print("distance:", result["distances"][0][i])
-    print("-" * 80)
+db_dir = len(sys.argv) >= 2 and sys.argv[1] or None
+if db_dir:
+    db = chromadb.PersistentClient(path=os.path.join(base_dir, db_dir))
+    collection = len(sys.argv) == 3 and sys.argv[2] or "default"
+    chroma_collection = db.get_collection(collection)
 
-vectorstore = Chroma(
-    client=db,
-    collection_name=collection,
-)
+    vectors = chroma_collection.peek(1)
+    vector = vectors["embeddings"][0]
+    result = chroma_collection.query(vector, n_results=2)
+    print("-" * 33, "chroma query", "-" * 33)
+    for i in range(len(result["ids"][0])):
+        print("Text:", result["documents"][0][i][:60])
+        print("distance:", result["distances"][0][i])
+        print("-" * 80)
 
-docs = vectorstore.similarity_search_by_vector(vector, k=2)
-print("-" * 30, "vector store query", "-" * 30)
-for doc in docs:
-    print(doc.page_content[:80])
-    print("-" * 80)
+    vectorstore = Chroma(
+        client=db,
+        collection_name=collection,
+    )
+
+    docs = vectorstore.similarity_search_by_vector(vector, k=2)
+    print("-" * 30, "vector store query", "-" * 30)
+    for doc in docs:
+        print(doc.page_content[:80])
+        print("-" * 80)
+
+else:
+    for subpath in os.listdir(base_dir):
+        print(subpath)
