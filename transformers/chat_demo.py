8a0c60a8b4761c4959972beead7dd83fff36a0ad
Jianshao <787903+jianshaow@users.noreply.github.com>
2024-11-28 10:01:35 +0000

    add printing

--- transformers/chat_demo.py
+++ transformers/chat_demo.py
@@ -3,14 +3,16 @@ import common, prompts, models
 model_name = common.hf_chat_model
 tokenizer = models.new_tokenizer(model_name)
 model = models.new_model(model_name)
+print("-" * 80)
+print("chat model:", model_name)
 
-prompt = prompts.tokenizer_prompt(
+messages = prompts.tokenizer_prompt(
     tokenizer,
     system_prompt="You are a pirate with a colorful personality.",
     user_prompt="what is your name?",
 )
 
-response = models.generate(model, tokenizer, prompt, streaming=True)
+response = models.generate(model, tokenizer, messages, streaming=True)
 
 print("-" * 80)
 for chunk in response:
