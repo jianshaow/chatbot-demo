6ad189cc53987d02ebd5edd7065a226a1616ee69
Jianshao <787903+jianshaow@users.noreply.github.com>
2024-11-28 09:27:58 +0000

    use chat model

--- langchain/hgface/llm_demo.py
+++ langchain/hgface/llm_demo.py
@@ -1,14 +1,9 @@
-from langchain_huggingface import HuggingFacePipeline
-from langchain_core.prompts import PromptTemplate
+from langchain_huggingface import HuggingFacePipeline, ChatHuggingFace
+from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 
 from common import hf_chat_model as model_name
 from common.models import default_model_kwargs
-
-prompt = PromptTemplate(
-    template="You are a pirate with a colorful personality. USER: {input}",
-    input_variables=["input"],
-)
 
 model_kwargs = default_model_kwargs()
 
@@ -16,13 +11,17 @@ llm = HuggingFacePipeline.from_model_id(
     model_id=model_name,
     task="text-generation",
     model_kwargs=model_kwargs,
-    pipeline_kwargs={"max_new_tokens": 256},
+    pipeline_kwargs={"max_new_tokens": 512},
 )
+model = ChatHuggingFace(llm=llm)
 print("-" * 80)
 print("chat model:", model_name)
 
+prompt = ChatPromptTemplate.from_messages(
+    [("system", "You are a pirate with a colorful personality."), ("user", "{input}")]
+)
 output_parser = StrOutputParser()
-chain = prompt | llm | output_parser
+chain = prompt | model | output_parser
 
 print("-" * 80)
 response = chain.stream({"input": "What is your name?"})
