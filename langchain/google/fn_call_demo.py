b089451277f14b5a355f636e81cda890a4af9f6b
Jianshao <787903+jianshaow@users.noreply.github.com>
2024-11-29 08:20:15 +0000

    extract common code

--- langchain/google/fn_call_demo.py
+++ langchain/google/fn_call_demo.py
@@ -1,34 +1,7 @@
-from langchain_core.messages import HumanMessage
 from langchain_google_genai import ChatGoogleGenerativeAI
 
-from common import google_fc_model as model
-from common.fn_tools import tools
-from common.functions import fns
+from common import google_fc_model as model_name
+from common.models import demo_fn_call
 
-llm = ChatGoogleGenerativeAI(model=model, transport="rest")
-llm_with_tools = llm.bind_tools(tools)
-
-messages = [HumanMessage("What is (121 * 3) + 42?")]
-response = llm_with_tools.invoke(messages)
-
-while response.tool_calls:
-    print("-" * 80)
-    messages.append(response)
-
-    for tool_call in response.tool_calls:
-        fn = fns[tool_call["name"]]
-        print("=== Calling Function ===")
-        print(
-            "Calling function:",
-            tool_call["name"],
-            "with args:",
-            tool_call["args"],
-        )
-        fn_result = fn.invoke(tool_call)
-        print("Got output:", fn_result.content)
-        print("========================\n")
-        messages.append(fn_result)
-    response = llm_with_tools.invoke(messages)
-
-print("-" * 80)
-print(response.content)
+fn_call_model = ChatGoogleGenerativeAI(model=model_name, transport="rest")
+demo_fn_call(fn_call_model, model_name)
