8994345cd39fb4b3d94f7902cdd42671c4c0a873
Jianshao <787903+jianshaow@users.noreply.github.com>
2024-12-03 07:03:06 +0000

    more complicated expression

--- llamaindex/openai/fn_call_agent_demo.py
+++ llamaindex/openai/fn_call_agent_demo.py
@@ -7,6 +7,6 @@ from common.fn_tools import tools
 llm = OpenAI(model=model)
 agent = OpenAIAgent.from_tools(tools, llm=llm, verbose=True)
 
-response = agent.chat("What is (121 * 3) + 42?")
+response = agent.chat("What is (121 * 3) + (6 * 7)?")
 print("-" * 80)
 print(str(response))
