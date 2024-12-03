7f3560f7020a084303a135e6858c8a20a69073e2
Jianshao <787903+jianshaow@users.noreply.github.com>
2024-12-03 04:07:29 +0000

    add few-shot for gemini

--- langchain/common/prompts.py
+++ langchain/common/prompts.py
@@ -11,18 +11,32 @@ system_prompt = SystemMessage(
 )
 
 examples = [
-    HumanMessage("What is (2 * 3) + 4"),
+    HumanMessage("What is (2 + 3) * 4"),
     AIMessage(
         "",
+        additional_kwargs={
+            "function_call": {
+                "id": "1",
+                "name": "add",
+                "arguments": '{"a": 2, "b": 3}',
+            }
+        },
+        tool_calls=[ToolCall({"id": "1", "name": "add", "args": {"a": 2, "b": 3}})],
+    ),
+    ToolMessage("5", tool_call_id="1"),
+    AIMessage(
+        "",
+        additional_kwargs={
+            "function_call": {
+                "id": "2",
+                "name": "multiply",
+                "arguments": '{"a": 5, "b": 4}',
+            }
+        },
         tool_calls=[
-            ToolCall({"id": "1", "name": "multiply", "args": {"a": 2, "b": 3}})
+            ToolCall({"id": "2", "name": "multiply", "args": {"a": 5, "b": 4}})
         ],
     ),
-    ToolMessage("6", tool_call_id="1"),
-    AIMessage(
-        "",
-        tool_calls=[ToolCall({"id": "2", "name": "add", "args": {"a": 6, "b": 4}})],
-    ),
-    ToolMessage("10", tool_call_id="2"),
+    ToolMessage("20", tool_call_id="2"),
     AIMessage("(2 * 3) + 4 = 10."),
 ]
