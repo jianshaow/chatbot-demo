f02fb0d94a568e81cdffb2a7844d110ba7899fa3
Jianshao <787903+jianshaow@users.noreply.github.com>
2024-12-09 06:53:08 +0000

    update

--- langchain/rag_chat/rag_config.py
+++ langchain/rag_chat/rag_config.py
@@ -68,7 +68,7 @@ class RagChatConfig:
 
     @property
     def vector_db_collection(self):
-        escaped = self.embed_model_name.replace(":", "-")
+        escaped = self.embed_model_name.replace(":", "_")
         return self.__data_dir + "__" + escaped
 
     def embed_model(self):
