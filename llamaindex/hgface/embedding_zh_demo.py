368dff261d8dcaee8a6287befd456309e6ec8734
Jianshao <787903+jianshaow@users.noreply.github.com>
2024-12-16 08:04:43 +0000

    support zh

--- llamaindex/hgface/embedding_zh_demo.py
+++ llamaindex/hgface/embedding_zh_demo.py
@@ -2,6 +2,6 @@ from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 
 from common.models import demo_embed
 
-model_name = "BAAI/bge-large-zh-v1.5"
+model_name = "BAAI/bge-small-zh-v1.5"
 embed_model = HuggingFaceEmbedding(model_name=model_name, trust_remote_code=True)
 demo_embed(embed_model, model_name, "地球发动机都安装在哪里？")
--- llamaindex/hgface/retriever_zh_demo.py
+++ llamaindex/hgface/retriever_zh_demo.py
@@ -2,6 +2,6 @@ from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 
 from common.models import demo_recieve
 
-model_name = "BAAI/bge-large-zh-v1.5"
+model_name = "BAAI/bge-small-zh-v1.5"
 embed_model = HuggingFaceEmbedding(model_name, trust_remote_code=True)
-demo_recieve(embed_model, model_name, "data_zh", "地球发动机都安装在哪里？")
+demo_recieve(embed_model, model_name, "data/zh-text", "地球发动机都安装在哪里？")
