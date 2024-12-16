6b9039f9bbcaa9db34246e3da7da8451f11db2fa
Jianshao <787903+jianshaow@users.noreply.github.com>
2024-12-16 17:16:22 +0000

    without few-shot

--- langchain/gemini/fn_call_demo.py
+++ langchain/gemini/fn_call_demo.py
@@ -4,4 +4,4 @@ from common import gemini_fc_model as model_name
 from common.models import demo_fn_call
 
 fn_call_model = ChatGoogleGenerativeAI(model=model_name, transport="rest")
-demo_fn_call(fn_call_model, model_name, with_few_shot=True)
+demo_fn_call(fn_call_model, model_name)
