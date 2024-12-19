17d9fc1a6a06b06857ca10afcd307c2f1d2aca60
Jianshao <787903+jianshaow@users.noreply.github.com>
2024-12-19 16:34:04 +0000

    suppress warning

--- llamaindex/hgface/chat_demo.py
+++ llamaindex/hgface/chat_demo.py
@@ -6,10 +6,9 @@ from common.models import default_model_kwargs, demo_chat
 model_kwargs = default_model_kwargs()
 
 chat_model = HuggingFaceLLM(
-    context_window=4096,
-    max_new_tokens=2048,
     tokenizer_name=model_name,
     model_name=model_name,
     model_kwargs=model_kwargs,
 )
+chat_model.generate_kwargs["pad_token_id"] = chat_model._tokenizer.eos_token_id
 demo_chat(chat_model, model_name)
