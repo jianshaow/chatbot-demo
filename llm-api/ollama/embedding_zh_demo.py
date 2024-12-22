93d139f490735d29b57bbca2554f1fcdcb6c4104
Jianshao <787903+jianshaow@users.noreply.github.com>
2024-12-22 13:10:27 +0000

    support zh

--- llm-api/ollama/embedding_zh_demo.py
+++ llm-api/ollama/embedding_zh_demo.py
@@ -1,11 +1,10 @@
 import ollama
 
-from common import ollama_embed_model as model_name
-from common.prompts import embed_question
-
+model_name = "paraphrase-multilingual:278m"
 print("-" * 80)
 print("embed model:", model_name)
 
+embed_question = "地球发动机都安装在哪里？"
 embedding = ollama.embed(model=model_name, input=embed_question)["embeddings"][0]
 
 print("-" * 80)
