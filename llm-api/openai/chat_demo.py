991dcb2e329e029b7d3e82d7f8bdee9d7e3afcbb
Jianshao <787903+jianshaow@users.noreply.github.com>
2025-02-06 07:21:08 +0000

    update

--- llm-api/openai/chat_demo.py
+++ llm-api/openai/chat_demo.py
@@ -1,21 +1,19 @@
-from openai import OpenAI
-
 from common import openai_chat_model as model
-from common.prompts import (
-    chat_system_message as system_prompt,
-    chat_question_message as question,
-)
+from common.prompts import chat_question_message as question
+from common.prompts import chat_system_message as system_prompt
+from openai import OpenAI, Stream
+from openai.types.chat import ChatCompletionChunk
 
 print("-" * 80)
 print("chat model:", model)
 
 client = OpenAI()
-response = client.chat.completions.create(
+response: Stream[ChatCompletionChunk] = client.chat.completions.create(
     model=model, messages=[system_prompt, question], stream=True
 )
 
 print("-" * 80)
 for chunk in response:
-    if chunk.choices[0].delta.content is not None:
-        print(chunk.choices[0].delta.content, end="")
+    if content := chunk.choices[0].delta.content:
+        print(content, end="")
 print("\n", "-" * 80, sep="")
