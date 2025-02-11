57b918c02fff7889a5c380ee228b52e29c1dc4f0
Jianshao <787903+jianshaow@users.noreply.github.com>
2025-02-11 12:53:06 +0000

    refactor

--- langchain/common/images.py
+++ langchain/common/images.py
@@ -10,15 +10,17 @@ from common.prompts import mm_image_url
 
 def show_demo_image():
     print("-" * 80)
-    response = requests.get(mm_image_url, timeout=2)
     print("demo image URL:", mm_image_url)
+    response = requests.get(mm_image_url, timeout=2)
+    assert response.status_code == 200
+    bytes_content = response.content
 
-    img = Image.open(BytesIO(response.content))
+    img = Image.open(BytesIO(bytes_content))
     plt.imshow(img)
     plt.show()
 
-    base64_content = base64.b64encode(response.content).decode("utf-8")
-    return response.content, base64_content
+    base64_content = base64.b64encode(bytes_content).decode("utf-8")
+    return bytes_content, base64_content
 
 
 if __name__ == "__main__":
