7b4c2010fd5061232e07fcd78f458bb463fba8a3
Jianshao <787903+jianshaow@users.noreply.github.com>
2025-02-18 15:32:22 +0000

    add refiner

--- transformers/common/__init__.py
+++ transformers/common/__init__.py
@@ -10,6 +10,9 @@ hf_chat_model = os.getenv("HF_CHAT_MODEL", "meta-llama/Llama-3.2-3B-Instruct")
 hf_mm_model = os.getenv("HF_MM_MODEL", "Qwen/Qwen2.5-VL-3B-Instruct")
 hf_dfs_model = os.getenv("HF_DFS_MODEL", "stabilityai/stable-diffusion-xl-base-1.0")
 hf_sd1_model = os.getenv("HF_SD1_MODEL", "stable-diffusion-v1-5/stable-diffusion-v1-5")
+hf_dfs_rf_model = os.getenv(
+    "HF_DFS_RF_MODEL", "stabilityai/stable-diffusion-xl-refiner-1.0"
+)
 hf_sd3_model = os.getenv("HF_SD3_MODEL", "stabilityai/stable-diffusion-3.5-medium")
 
 bnb_enabled = os.getenv("BNB_ENABLED", "false") == "true"
--- transformers/text_to_image/dfs_demo.py
+++ transformers/text_to_image/dfs_demo.py
@@ -2,16 +2,42 @@ import matplotlib.pyplot as plt
 import torch
 from diffusers import DiffusionPipeline
 
-from common import hf_dfs_model as model
+from common import hf_dfs_model as base_model
+from common import hf_dfs_rf_model as refiner_model
 
-pipe = DiffusionPipeline.from_pretrained(
-    model, torch_dtype=torch.float16, use_safetensors=True, variant="fp16"
+base = DiffusionPipeline.from_pretrained(
+    base_model, torch_dtype=torch.float16, use_safetensors=True, variant="fp16"
 )
-pipe.to("cuda")
+base.to("cuda")
+refiner = DiffusionPipeline.from_pretrained(
+    refiner_model,
+    text_encoder_2=base.text_encoder_2,
+    vae=base.vae,
+    torch_dtype=torch.float16,
+    use_safetensors=True,
+    variant="fp16",
+)
+refiner.to("cuda")
+
+n_steps = 40
+high_noise_frac = 0.8
 
 prompt = "An astronaut riding a green horse"
 
-image = pipe(prompt=prompt).images[0]
+image = base(
+    prompt=prompt,
+    num_inference_steps=n_steps,
+    denoising_end=high_noise_frac,
+    output_type="latent",
+).images
+
+image = refiner(
+    prompt=prompt,
+    num_inference_steps=n_steps,
+    denoising_start=high_noise_frac,
+    image=image,
+).images[0]
+
 plt.imshow(image)
 plt.show()
 # image.save("output/astronaut_rides_green_horse.png")
