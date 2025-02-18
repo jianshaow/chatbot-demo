1e4060d1d4fd51cc2a32e0df3c2357f1866d97b3
Jianshao <787903+jianshaow@users.noreply.github.com>
2025-02-18 06:58:12 +0000

    new base tag

--- transformers/build-image.sh
+++ transformers/build-image.sh
@@ -5,8 +5,13 @@ if [ "$torch_ver" == "" ]; then
 fi
 echo "Using torch version: ${torch_ver}"
 
+if [ "$cuda_tag" == "" ]; then
+    cuda_tag=cu124
+fi
+echo "Using cuda tag: ${cuda_tag}"
+
 image=jianshao/transformers-demo
-docker build -t ${image}:latest . --build-arg TAG=$torch_ver
+docker build -t ${image}:latest . --build-arg TAG=${torch_ver}-${cuda_tag}
 
 transformers_ver=$(docker run --rm ${image}:latest pip list | grep transformers | awk '{print $2}')
 echo "Using transformers version ${transformers_ver}"
