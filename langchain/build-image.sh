ed680cfa230845a040b77332c6d411921301a7d8
Jianshao <787903+jianshaow@users.noreply.github.com>
2025-02-18 07:14:15 +0000

    use transformers demo as base

--- langchain/build-image.sh
+++ langchain/build-image.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-if [ "$torch_ver" == "" ]; then
-    torch_ver=2.4.1
+if [ "$transformers_ver" == "" ]; then
+    transformers_ver=latest
 fi
-echo "Using torch version: ${torch_ver}"
+echo "Using transformers version: ${transformers_ver}"
 
-base_image=jianshao/torch-dev-base
-docker pull ${base_image}:${torch_ver}
+base_image=jianshao/transformers-demo
+docker pull ${base_image}:${transformers_ver}
 
 image=jianshao/langchain-demo
-docker build -t ${image}:latest . --build-arg TAG=$torch_ver $*
+docker build -t ${image}:latest . --build-arg TAG=$transformers_ver $*
 
 langchain_core_ver=$(docker run --rm ${image}:latest pip list | grep langchain-core | awk '{print $2}')
 echo "Using langchain-core version ${langchain_core_ver}"
