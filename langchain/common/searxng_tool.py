7628e7b9cacdecab0e96dc3007b85fd6928b9626
Jianshao <787903+jianshaow@users.noreply.github.com>
2025-03-21 09:51:31 +0000

    update

--- langchain/common/searxng_tool.py
+++ langchain/common/searxng_tool.py
@@ -1,17 +1,10 @@
-from langchain_community.agent_toolkits.load_tools import load_tools
 from langchain_community.tools.searx_search.tool import SearxSearchResults
 from langchain_community.utilities import SearxSearchWrapper
 
 from common import searxng_host
 
-tools = load_tools(
-    ["searx-search-results-json"],
-    searx_host=searxng_host,
-    engines=["github"],
-    num_results=1,
-)
-
 wrapper = SearxSearchWrapper(searx_host=searxng_host)
+
 github_tool = SearxSearchResults(
     name="Github",
     wrapper=wrapper,
