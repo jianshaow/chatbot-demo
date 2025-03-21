27830ab05849f2afc48c3c2cbaf948ab0b5d82ae
Jianshao <787903+jianshaow@users.noreply.github.com>
2025-03-21 10:38:59 +0000

    support basic auth

--- langchain/common/searxng_tool.py
+++ langchain/common/searxng_tool.py
@@ -1,9 +1,17 @@
 from langchain_community.tools.searx_search.tool import SearxSearchResults
 from langchain_community.utilities import SearxSearchWrapper
 
-from common import searxng_host
+from common import (
+    get_basic_auth_headers,
+    searxng_host,
+    searxng_password,
+    searxng_username,
+)
 
-wrapper = SearxSearchWrapper(searx_host=searxng_host)
+wrapper = SearxSearchWrapper(
+    searx_host=searxng_host,
+    headers=get_basic_auth_headers(searxng_username, searxng_password),
+)
 
 github_tool = SearxSearchResults(
     name="Github",
@@ -18,4 +26,5 @@ arxiv_tool = SearxSearchResults(
 )
 
 if __name__ == "__main__":
-    print(arxiv_tool.run("what is a large language model?"))
+    result = arxiv_tool.run("what is chain-of-thought?")
+    print(result)
