ef9265208b0953531e0f22383dd4dccdc91705b3
Jianshao <787903+jianshaow@users.noreply.github.com>
2025-03-31 10:23:58 +0000

    add mcp

---
+++ llamaindex/mcp/client.py
@@ -0,0 +1,29 @@
+import asyncio
+
+from llama_index.tools.mcp import McpToolSpec
+
+from mcp import ClientSession, StdioServerParameters
+from mcp.client.stdio import stdio_client
+
+server_params = StdioServerParameters(
+    command="python",
+    args=["mcp/server.py"],
+)
+
+
+async def get_tools_async():
+    async with stdio_client(server_params) as (read, write):
+        async with ClientSession(read, write) as session:
+            await session.initialize()
+            tools = await McpToolSpec(session).to_tool_list_async()
+            return tools
+
+
+def get_tools():
+    return asyncio.run(get_tools_async())
+
+
+if __name__ == "__main__":
+    mcp_tools = get_tools()
+    for tool in mcp_tools:
+        print(tool.metadata.name, ":", tool.metadata.description)
