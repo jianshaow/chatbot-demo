cfdce7c637ea8b21828f0e9b09b33088262c62d5
Jianshao <787903+jianshaow@users.noreply.github.com>
2025-04-24 05:32:18 +0000

    rename

--- langchain/google/fn_call_mcp_demo.py
+++ langchain/google/fn_call_mcp_demo.py
@@ -2,7 +2,7 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 
 from common import google_fc_model as model
 from common.models import demo_fn_call
-from mcp_tools import get_tools
+from mcp_tools import get_stdio_tools
 
 fn_call_model = ChatGoogleGenerativeAI(model=model, transport="rest")
-demo_fn_call(fn_call_model, model, tools=get_tools(), with_few_shot=True)
+demo_fn_call(fn_call_model, model, tools=get_stdio_tools(), with_few_shot=True)
--- langchain/ollama/fn_call_mcp_demo.py
+++ langchain/ollama/fn_call_mcp_demo.py
@@ -3,7 +3,7 @@ from langchain_ollama import ChatOllama
 from common import ollama_base_url as base_url
 from common import ollama_fc_model as model
 from common.models import demo_fn_call
-from mcp_tools import get_tools
+from mcp_tools import get_stdio_tools
 
 fn_call_model = ChatOllama(base_url=base_url, model=model)
-demo_fn_call(fn_call_model, model, tools=get_tools(), with_few_shot=True)
+demo_fn_call(fn_call_model, model, tools=get_stdio_tools(), with_few_shot=True)
