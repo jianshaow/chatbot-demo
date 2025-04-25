4a4ba143906903feb80dfbbb00edc5f5ef74ea71
Jianshao <787903+jianshaow@users.noreply.github.com>
2025-04-25 08:18:31 +0000

    use sse mcp tools

--- langchain/openai/fn_call_mcp_demo.py
+++ langchain/openai/fn_call_mcp_demo.py
@@ -2,7 +2,7 @@ from langchain_openai import ChatOpenAI
 
 from common import openai_fc_model as model_name
 from common.models import demo_fn_call
-from mcp_tools import get_stdio_tools
+from mcp_tools import get_stdio_tools as get_tools
 
 fn_call_model = ChatOpenAI(model=model_name)
-demo_fn_call(fn_call_model, model_name, tools=get_stdio_tools())
+demo_fn_call(fn_call_model, model_name, tools=get_tools())
