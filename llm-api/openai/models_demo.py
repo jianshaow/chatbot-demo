a068c6a3487cea08b5f2cfa827b884762d5015b7
Jianshao <787903+jianshaow@users.noreply.github.com>
2025-06-05 06:22:06 +0000

    add filter

--- llm-api/openai/models_demo.py
+++ llm-api/openai/models_demo.py
@@ -1,3 +1,4 @@
+import os
 import sys
 
 from clients import get_client
@@ -6,12 +7,13 @@ from dotenv import load_dotenv
 load_dotenv()
 
 verbose = len(sys.argv) > 1 and sys.argv[1] == "verbose" or False
+owned_by = os.getenv("OWNED_BY", "openai")
 
 client = get_client()
 models = client.models.list()
 print("-" * 80)
 for model in models:
-    if model.owned_by != "openai":
+    if model.owned_by != owned_by:
         continue
     if verbose:
         print(model)
