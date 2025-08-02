21b5fb743cbaf3f89163aa34d03619a2a6924990
Jianshao <787903+jianshaow@users.noreply.github.com>
2025-08-02 15:25:08 +0000

    comment temporarily

--- llamaindex/hgface/multi_modal_demo.py
+++ llamaindex/hgface/multi_modal_demo.py
@@ -1,13 +1,13 @@
-from llama_index.multi_modal_llms.huggingface import HuggingFaceMultiModal
+# from llama_index.multi_modal_llms.huggingface import HuggingFaceMultiModal
 
-from common import hf_mm_model as model_name
-from common.images import show_demo_image
-from common.models import default_model_kwargs, demo_multi_modal_legacy
+# from common import hf_mm_model as model_name
+# from common.images import show_demo_image
+# from common.models import default_model_kwargs, demo_multi_modal_legacy
 
-model_kwargs = default_model_kwargs()
+# model_kwargs = default_model_kwargs()
 
-show_demo_image()
-mm_model = HuggingFaceMultiModal.from_model_name(
-    model_name, trust_remote_code=True, additional_kwargs=model_kwargs
-)
-demo_multi_modal_legacy(mm_model, model_name, streaming=False)
+# show_demo_image()
+# mm_model = HuggingFaceMultiModal.from_model_name(
+#     model_name, trust_remote_code=True, additional_kwargs=model_kwargs
+# )
+# demo_multi_modal_legacy(mm_model, model_name, streaming=False)
