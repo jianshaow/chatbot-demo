ccdf0f81d180d5c3096fb8beaae58e1e2369e19c
Jianshao <787903+jianshaow@users.noreply.github.com>
2025-08-17 13:56:19 +0000

    use original ollama embed model

--- llamaindex/ollama/retriever_demo.py
+++ llamaindex/ollama/retriever_demo.py
@@ -1,7 +1,6 @@
-from common.ollama import NormOllamaEmbedding
-
-from common import ollama_base_url as base_url, ollama_embed_model as model_name
+from common import ollama_embed_model as model_name
 from common.models import demo_retrieve
+from common.ollama import get_embed_model
 
-embed_model = NormOllamaEmbedding(base_url=base_url, model_name=model_name)
+embed_model = get_embed_model(model_name=model_name)
 demo_retrieve(embed_model, model_name)
--- llamaindex/ollama/retriever_zh_demo.py
+++ llamaindex/ollama/retriever_zh_demo.py
@@ -1,8 +1,6 @@
-from common.ollama import NormOllamaEmbedding
-
-from common import ollama_base_url as base_url
 from common.models import demo_retrieve
+from common.ollama import get_embed_model
 
 model_name = "paraphrase-multilingual:278m"
-embed_model = NormOllamaEmbedding(base_url=base_url, model_name=model_name)
+embed_model = get_embed_model(model_name=model_name)
 demo_retrieve(embed_model, model_name, "data/zh-text", "地球发动机都安装在哪里？")
