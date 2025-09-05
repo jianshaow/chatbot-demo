a01f50efc7a83eb31f3bef2b6c119a14d2f8a35b
Jianshao <787903+jianshaow@users.noreply.github.com>
2025-09-05 09:01:53 +0000

    update

--- llm-api/ollama/chat_demo.py
+++ llm-api/ollama/chat_demo.py
@@ -1,10 +1,8 @@
 import ollama
-
-from common import ollama_chat_model as model, think
-from common.prompts import (
-    chat_system_message as system_prompt,
-    chat_question_message as question,
-)
+from common import ollama_chat_model as model
+from common import think
+from common.prompts import chat_question_message as question
+from common.prompts import chat_system_message as system_prompt
 
 print("-" * 80)
 print("chat model:", model)
@@ -15,5 +13,5 @@ response = ollama.chat(
 
 print("-" * 80)
 for chunk in response:
-    print(chunk["message"]["content"], end="", flush=True)
+    print(chunk.message.content, end="", flush=True)
 print("\n", "-" * 80, sep="")
