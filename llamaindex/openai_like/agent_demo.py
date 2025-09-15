07e8a217acdc9253f45080b867f3248b4cabad1f
Jianshao <787903+jianshaow@users.noreply.github.com>
2025-09-15 09:53:24 +0000

    add agent demo

---
+++ llamaindex/openai_like/agent_demo.py
@@ -0,0 +1,8 @@
+from common import openai_like_chat_model as chat_model_name
+from common import openai_like_embed_model as embed_model_name
+from common.models import demo_agent
+from common.openai_like import get_embed_model, get_llm
+
+embed_model = get_embed_model(model_name=embed_model_name)
+chat_model = get_llm(model=chat_model_name)
+demo_agent(embed_model, embed_model_name, chat_model, chat_model_name)
