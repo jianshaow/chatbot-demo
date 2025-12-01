c16e1ca460be9e06a2c15f6921bcea664c7eba4f
Jianshao <787903+jianshaow@users.noreply.github.com>
2025-12-01 07:24:58 +0000

    rename

--- llamaindex/mcp_tools/context7_client.py
+++ llamaindex/mcp_tools/context7_client.py
@@ -5,19 +5,19 @@ from llama_index.tools.mcp import BasicMCPClient, McpToolSpec
 from mcp.types import CallToolResult, TextContent
 
 
-async def get_sse_tools_async():
+async def get_http_tools_async():
     mcp_client = BasicMCPClient("https://mcp.context7.com/mcp")
     mcp_tool_spec = McpToolSpec(client=mcp_client)
 
     return await mcp_tool_spec.to_tool_list_async()
 
 
-def get_sse_tools():
-    return asyncio.run(get_sse_tools_async())
+def get_http_tools():
+    return asyncio.run(get_http_tools_async())
 
 
 if __name__ == "__main__":
-    mcp_tools = get_sse_tools()
+    mcp_tools = get_http_tools()
     for tool in mcp_tools:
         tool_name = tool.metadata.name
         print("tool name:", tool_name)
