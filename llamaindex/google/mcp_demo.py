378fb5e75ef93cc0d15f670dbbba394a6357487c
Jianshao <787903+jianshaow@users.noreply.github.com>
2025-12-01 07:25:08 +0000

    add google mcp demo

---
+++ llamaindex/google/mcp_demo.py
@@ -0,0 +1,13 @@
+from llama_index.llms.google_genai import GoogleGenAI
+
+from common import google_fc_model as model
+from common.models import demo_fn_call_agent
+from mcp_tools.context7_client import get_http_tools as get_tools
+
+fn_call_model = GoogleGenAI(model=model)
+demo_fn_call_agent(
+    fn_call_model,
+    model,
+    tools=get_tools(),
+    query="what is latest version of spring-boot?",
+)
