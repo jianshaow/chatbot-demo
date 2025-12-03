9ebe2554aa35e68cfa2827a8ea4eca6e3666cb66
Jianshao <787903+jianshaow@users.noreply.github.com>
2025-12-03 17:19:26 +0000

    fix import

--- llamaindex/ollama/fn_call_mcp_demo.py
+++ llamaindex/ollama/fn_call_mcp_demo.py
@@ -1,7 +1,7 @@
 from common import ollama_fc_model as model
 from common.models import demo_fn_call
 from common.ollama import get_llm
-from mcp_tools import get_calc_stdio_tools
+from mcp_tools.calc_client import get_calc_stdio_tools
 
 fn_call_model = get_llm(model=model)
 demo_fn_call(fn_call_model, model, tools=get_calc_stdio_tools(), with_few_shot=True)
