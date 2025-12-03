0bff55349953ca931a1f3bcfd79a1d5bfbc9efec
Jianshao <787903+jianshaow@users.noreply.github.com>
2025-12-03 17:33:16 +0000

    add mcp demo

---
+++ llamaindex/ollama/mcp_demo.py
@@ -0,0 +1,12 @@
+from common import ollama_fc_model as model
+from common.models import demo_fn_call_agent
+from common.ollama import get_llm
+from mcp_tools.context7_client import get_http_tools as get_tools
+
+fn_call_model = get_llm(model=model)
+demo_fn_call_agent(
+    fn_call_model,
+    model,
+    tools=get_tools(),
+    query="what is latest version of spring-boot?",
+)
