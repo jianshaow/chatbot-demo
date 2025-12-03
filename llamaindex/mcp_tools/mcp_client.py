baed7d80451cf4d5bb6f0cbe52e19ced22e65b14
Jianshao <787903+jianshaow@users.noreply.github.com>
2025-12-03 10:42:28 +0000

    add mcp client demo

---
+++ llamaindex/mcp_tools/mcp_client.py
@@ -0,0 +1,45 @@
+import asyncio
+from typing import List, Optional
+
+from llama_index.tools.mcp import BasicMCPClient
+
+
+def get_docker_mcp_client(
+    image: str,
+    extra_docker_args: List[str] | None = None,
+    command: str | None = None,
+    args: List[str] | None = None,
+):
+    mcp_command = "docker"
+    mcp_args = ["run", "--rm", "-i"]
+    if extra_docker_args:
+        mcp_args.extend(extra_docker_args)
+    mcp_args.append(image)
+    if command:
+        mcp_args.append(command)
+    if args:
+        mcp_args.extend(args)
+    return BasicMCPClient(mcp_command, args=mcp_args)
+
+
+async def call_tool(
+    client: BasicMCPClient,
+    tool_name: str,
+    arguments: Optional[dict] = None,
+):
+    result = await client.call_tool(tool_name, arguments=arguments)
+    print(result)
+
+
+if __name__ == "__main__":
+    mcp_client = get_docker_mcp_client(
+        "mcp/filesystem",
+        [
+            "-v",
+            "/:/workspace",
+        ],
+        args=["/workspace"],
+    )
+    asyncio.run(
+        call_tool(mcp_client, "list_directory", arguments={"path": "/workspace"})
+    )
