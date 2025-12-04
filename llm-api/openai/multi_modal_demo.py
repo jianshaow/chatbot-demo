0a5780b0ca14365e2da814045d2aaab390eb5e3d
Jianshao <787903+jianshaow@users.noreply.github.com>
2025-12-04 17:01:24 +0000

    cleanup

--- llm-api/openai/multi_modal_demo.py
+++ llm-api/openai/multi_modal_demo.py
@@ -1,20 +1,15 @@
-from typing import cast
-
 from common import openai_mm_model as model
 from common.openai import get_client
 from common.prompts import mm_question_message
 from openai import Stream
-from openai.types.chat import ChatCompletionChunk, ChatCompletionUserMessageParam
+from openai.types.chat import ChatCompletionChunk
 
 print("-" * 80)
 print("multi-modal model:", model)
 
 client = get_client()
-user_message = cast(ChatCompletionUserMessageParam, mm_question_message)
 response: Stream[ChatCompletionChunk] = client.chat.completions.create(
-    model=model,
-    messages=[user_message],
-    stream=True,
+    model=model, messages=[mm_question_message], stream=True
 )
 
 print("-" * 80)
