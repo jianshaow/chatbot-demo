a03731ae9997880572687d2812004be77ff79cc0
Jianshao <787903+jianshaow@users.noreply.github.com>
2026-01-29 03:21:22 +0000

    cleanup

--- langchain/openai/fn_call_mcp_demo.py
+++ langchain/openai/fn_call_mcp_demo.py
@@ -1,7 +1,7 @@
 from common import openai_fc_model as model_name
 from common.models import demo_fn_call
 from common.openai import get_chat_model
-from mcp_tools import get_stdio_tools as get_tools
+from mcp_tools.calc_client import get_stdio_tools as get_tools
 
 fn_call_model = get_chat_model(model=model_name)
 demo_fn_call(fn_call_model, model_name, tools=get_tools())
