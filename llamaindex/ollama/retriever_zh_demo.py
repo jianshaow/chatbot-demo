95ccb14e43b081c409ae2ddf0c90c29ac928dc96
Jianshao <787903+jianshaow@users.noreply.github.com>
2026-02-07 10:25:15 +0000

    unify embedding model

--- llamaindex/ollama/retriever_zh_demo.py
+++ llamaindex/ollama/retriever_zh_demo.py
@@ -1,6 +1,6 @@
+from common import ollama_embed_model as embed_model_name
 from common.models import demo_retrieve
 from common.ollama import get_embed_model
 
-model_name = "paraphrase-multilingual:278m"
-embed_model = get_embed_model(model_name=model_name)
+embed_model = get_embed_model(model_name=embed_model_name)
 demo_retrieve(embed_model, "data/zh-text", "地球发动机都安装在哪里？")
