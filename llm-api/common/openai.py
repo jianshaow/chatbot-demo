ac3ba83473dba195d86965dbc1e9b7baa8c397d6
Jianshao <787903+jianshaow@users.noreply.github.com>
2026-08-13 06:03:48 +0000

    complete agent

--- llm-api/common/openai.py
+++ llm-api/common/openai.py
@@ -2,6 +2,7 @@ import json
 import os
 
 import httpx
+from agents import Agent, Runner, RunResult, RunResultStreaming
 
 from common import ssl_verify
 from openai import OpenAI
@@ -16,3 +17,15 @@ def get_client():
 def __get_extra_headers():
     extra_headers_env = os.getenv("EXTRA_HEADERS", "{}")
     return json.loads(extra_headers_env)
+
+
+def get_agent(*args, **kwargs) -> Agent:
+    return Agent(*args, **kwargs)
+
+
+def run_agent(agent: Agent, prompt: str) -> RunResult:
+    return Runner.run_sync(agent, prompt)
+
+
+def run_streamed_agent(agent: Agent, prompt: str) -> RunResultStreaming:
+    return Runner.run_streamed(agent, prompt)
--- llm-api/openai/agent_demo.py
+++ llm-api/openai/agent_demo.py
@@ -1,8 +1,27 @@
-from agents import Agent, Runner
+from agents import RawResponsesStreamEvent
 
 from common import openai_chat_model as model
+from common.openai import get_agent, run_streamed_agent
+from openai.types.responses import ResponseTextDeltaEvent
 
-agent = Agent(model=model, name="Assistant", instructions="You are a helpful assistant")
 
-result = Runner.run_sync(agent, "Write a haiku about recursion in programming.")
-print(result.final_output)
+async def main():
+    agent = get_agent(
+        model=model, name="Assistant", instructions="You are a helpful assistant"
+    )
+    print("-" * 80)
+    print("chat model:", model)
+    result = run_streamed_agent(agent, "Write a haiku about recursion in programming.")
+    print("-" * 80)
+    async for event in result.stream_events():
+        if isinstance(event, RawResponsesStreamEvent) and isinstance(
+            event.data, ResponseTextDeltaEvent
+        ):
+            print(event.data.delta, end="", flush=True)
+    print("\n", "-" * 80, sep="")
+
+
+if __name__ == "__main__":
+    import asyncio
+
+    asyncio.run(main())
