735166be99c55ed369ed97a88a8e0a88f91102fd
Jianshao <787903+jianshaow@users.noreply.github.com>
2026-08-25 03:46:34 +0000

    cleanup

--- langchain/google/embedding_demo.py
+++ langchain/google/embedding_demo.py
@@ -2,5 +2,5 @@ from common import google_embed_model as model_name
 from common.google import get_embed_model
 from common.models import demo_embed
 
-embed_model = get_embed_model(model=model_name, transport="rest")
+embed_model = get_embed_model(model=model_name)
 demo_embed(embed_model, model_name)
